f3857dc0a875278add72ca184e16d825d1ae3c85
bvvy <bvvy@qq.com>
2017-11-02 23:07:57 +0800

    arraylist

---
+++ src/data_structure/MArrayList.java
@@ -0,0 +1,16 @@
+package data_structure;
+
+public class MArrayList<Item> {
+
+    private Item[] items;
+    private int N;
+
+    public MArrayList(int N) {
+        this.N = N;
+        this.items = (Item[]) new Object[N];
+    }
+
+    public void add(Item item) {
+
+    }
+}
